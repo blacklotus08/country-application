887e22e9c6c6554b9444b91a4b5d4effa64de7c3
blacklotus08 <jettorres08@gmail.com>
2023-04-05 18:49:48 +0800

    feat: Created neccessary components

--- country-app/src/app/app.module.ts
+++ country-app/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CountryListComponent } from './country/country-list/country-list.component';
+import { CountryDetailsComponent } from './country/country-details/country-details.component';
+import { AddCountryComponent } from './country/add-country/add-country.component';
+import { EditCountryComponent } from './country/edit-country/edit-country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryListComponent,
+    CountryDetailsComponent,
+    AddCountryComponent,
+    EditCountryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ country-app/src/app/country/models/country.model.ts
@@ -0,0 +1,8 @@
+
+export interface Country {
+    _id: number;
+    country_code: string;
+    country_name: string;
+    country_region: string;
+}
+
